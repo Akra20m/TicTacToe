74db8bfb8191731296f83d68f24c981b3e67adb9
Akra20m <akra200m@gmail.com>
2020-03-12 14:34:59 -0400

    Updates

---
+++ precache-manifest.98c13ed1fe9272d677757d3080dad868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd412f786eec8b3a1266beb8c32d756",
+    "url": "/TicTacToe/index.html"
+  },
+  {
+    "revision": "fefebb434755a0c9adb5",
+    "url": "/TicTacToe/static/css/main.4d278991.chunk.css"
+  },
+  {
+    "revision": "1be2132afe8a167be818",
+    "url": "/TicTacToe/static/js/2.75cf627c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/TicTacToe/static/js/2.75cf627c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fefebb434755a0c9adb5",
+    "url": "/TicTacToe/static/js/main.3bf3ea59.chunk.js"
+  },
+  {
+    "revision": "525bdc2bfdd4d50f3c3f",
+    "url": "/TicTacToe/static/js/runtime-main.44dc4a05.js"
+  }
+]);
